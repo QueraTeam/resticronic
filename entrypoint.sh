ba865ab88bc85e0b2612d8ece313b02c6760e46e
Mohammad Javad Naderi <mjnaderi@gmail.com>
2025-11-03 17:34:25 +0330

    Fix condition check for command arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [ -n "${RESTICRONIC_CRONTAB}" ]; then
     echo "${RESTICRONIC_CRONTAB}" >/etc/resticronic/crontab
     echo "Created /etc/resticronic/crontab from RESTICRONIC_CRONTAB environment variable."
-    if [ -z "$@" ]; then
+    if [ "$#" -eq 0 ]; then
         set -- supercronic /etc/resticronic/crontab
     fi
 fi
